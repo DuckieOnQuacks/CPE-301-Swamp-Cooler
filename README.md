70fcc64a837c9f5f64d1b8e1ce833b445fa7cb90
Joey Paschke <58445029+DuckieOnQuacks@users.noreply.github.com>
2022-02-23 02:07:28 -0800

    working clock code for time requirments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CPE 301 Swamp Cooler
- Code for the swamp cooler final project for CPE 301
+# cpe-301-semester-project-group-4
+Group Members: Joey Paschke, Crystal Atoz, Britanny Guzman
+https://docs.google.com/document/d/1PMsQEBuNnK6mM40WEHknzZ9ZdnxIn3VqlyBwdnXil3s/edit?usp=sharing
---
+++ Swamp_Cooler_Clock_Setup.ino
@@ -0,0 +1,37 @@
+#include <RTClib.h>
+#include <Arduino.h>
+RTC_DS1307 clock;
+void setup() 
+{
+    // Set Serial baud rate
+    Serial.begin(9600);
+    
+    // Initialize DS1307
+    clock.begin();
+    clock.adjust(DateTime(F(__DATE__), F(__TIME__)));
+}
+
+void loop() 
+{
+    Serial.print(" -> ");
+    //Serial.print(state);
+    Serial.print(" occurred at ");
+    printTime();
+}
+
+void printTime()
+{
+    DateTime now = clock.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+}
